1aa9734c1cc94aa21291d3ff9d3f8b176a846719
nhishon <nhishon@luc.edu>
2019-03-26 19:54:32 -0500

    assets yo

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module UniApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.compile = true
+    config.serve_static_assets = true
     # config.assets.initalize = false
     # config.assets.compile = false
 
